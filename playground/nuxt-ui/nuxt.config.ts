c9f1949f83212b8c1498e3f7a54dc308162468c5
Shimpei Iwamaru <gangan.education@gmail.com>
2024-03-15 07:15:28 +0900

    feat(ui): add nuxt-og-image module

--- playground/nuxt-ui/nuxt.config.ts
+++ playground/nuxt-ui/nuxt.config.ts
@@ -1,4 +1,4 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/image"],
+  modules: ["@nuxt/ui", "@nuxt/image", "nuxt-og-image", "@nuxtjs/seo"],
 });
