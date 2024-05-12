31e56b101455159cd95a0926120a07d64c089798
Shimpei Iwamaru <gangan.education@gmail.com>
2024-05-13 03:07:33 +0900

    feat(eslint): build Nuxt ESLint and update deps

---
+++ playground/eslint/eslint.config.mjs
@@ -0,0 +1,21 @@
+// @ts-check
+import globals from "globals";
+import withNuxt from "./.nuxt/eslint.config.mjs";
+
+export default withNuxt({
+  ignores: [
+    "**/coverage",
+    // Note: 以下はdefaultで設定済み
+    // - ref: https://github.com/nuxt/eslint/blob/main/packages/eslint-config/src/flat/configs/ignores.ts
+    // '**/dist', '**/node_modules', '**/.nuxt', '**/.output',
+  ],
+  globals: {
+    ...globals.browser,
+    ...globals.node,
+  },
+});
+// .override("nuxt/typescript", {
+//   rules: {
+//     "@typescript-eslint/explicit-function-return-type": "error",
+//   },
+// });
